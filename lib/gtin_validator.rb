5b86aa2545bef53825b4fe36bc060505e99ea35b
graefs <graefalan@gmail.com>
2018-11-26 20:33:39 -0300

    Add code and tests

--- lib/gtin_validator.rb
+++ lib/gtin_validator.rb
@@ -1,5 +1,79 @@
 require "gtin_validator/version"
 
 module GtinValidator
-  # Your code goes here...
+  class << self
+    def valid_gtin?(number)
+      number = number.to_s.reverse # Transform the number into String to invert.
+      return false unless [8,12,13,14].include? number.length
+      odds = evens = 0
+
+      # Skip the first number (check digit)
+      (1..number.length - 1).each do |n|
+        # Add number to evens or odds
+        n.even? ? (evens += number[n].chr.to_i) : (odds += number[n].chr.to_i)
+      end
+
+      # Check if check digit == result of check digit operation
+      number[0].chr.to_i == ((10 - ((odds * 3) + evens)) % 10)
+    end
+
+    def calculate_checkdigit(number)
+      number = number.to_s.reverse
+      return false unless [7,11,12,13].include? number.length
+      odds = evens = 0
+
+      (0..number.length - 1).each do |n|
+        i = n + 1
+        i.even? ? (evens += number[n].chr.to_i) : (odds += number[n].chr.to_i)
+      end
+
+      ((10 - ((odds * 3) + evens)) % 10)
+    end
+
+    def what_gtin_is?(number)
+      case number.to_s.length
+      when 8
+        gtin_8?(number.to_s) ? "GTIN-8" : false
+      when 12
+        gtin_12?(number.to_s) ? "GTIN-12" : false
+      when 13
+        gtin_13?(number.to_s) ? "GTIN-13" : false
+      when 14
+        gtin_14?(number.to_s) ? "GTIN-14" : false
+      else
+        false
+      end
+    end
+
+    def gtin_14?(gtin)
+      return false if gtin.to_s.length != 14
+      valid_gtin? gtin
+    end
+    alias is_gtin_14? gtin_14?
+
+    def gtin_13?(gtin)
+      return false if gtin.to_s.length != 13
+      valid_gtin? gtin
+    end
+    alias ean? gtin_13?
+    alias is_ean? gtin_13?
+    alias ean_13? gtin_13?
+    alias is_gtin_13? gtin_13?
+
+    def gtin_12?(gtin)
+      return false if gtin.to_s.length != 12
+      valid_gtin? gtin
+    end
+    alias upc? gtin_12?
+    alias is_upc? gtin_12?
+    alias upc_12? gtin_12?
+    alias is_gtin_12? gtin_12?
+
+    def gtin_8?(gtin)
+      return false if gtin.to_s.length != 8
+      valid_gtin? gtin
+    end
+    alias ean_8? gtin_8?
+    alias is_gtin_8? gtin_8?
+  end
 end
--- spec/gtin_validator_spec.rb
+++ spec/gtin_validator_spec.rb
@@ -5,7 +5,142 @@ describe GtinValidator do
     expect(GtinValidator::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#valid_gtin?' do
+    it 'correct GTIN-14 should be true' do
+      expect(GtinValidator.valid_gtin? "00012345600012").to be_truthy
+    end
+    it 'correct GTIN-13 should be true' do
+      expect(GtinValidator.valid_gtin? "0123456789012").to be_truthy
+    end
+    it 'correct GTIN-12 should be true' do
+      expect(GtinValidator.valid_gtin? "012345678905").to be_truthy
+    end
+    it 'correct GTIN-8 should be true' do
+      expect(GtinValidator.valid_gtin? "96385074").to be_truthy
+    end
+    it 'random numbers (invalid GTIN) should be false' do
+      expect(GtinValidator.valid_gtin? "123456").to be_falsey
+    end
+  end
+
+  describe '#calculate_checkdigit' do
+    it 'whit correct string should see checkdigit' do
+      expect(GtinValidator.calculate_checkdigit "0001234560001").to eq(2)
+      expect(GtinValidator.calculate_checkdigit "012345678901").to eq(2)
+      expect(GtinValidator.calculate_checkdigit "01234567890").to eq(5)
+      expect(GtinValidator.calculate_checkdigit "9505000").to eq(3)
+    end
+
+    it 'whit incorrect string should see false' do
+      expect(GtinValidator.calculate_checkdigit "01234567").to be_falsey
+      expect(GtinValidator.calculate_checkdigit "012").to be_falsey
+    end
+  end
+
+  describe '#what_gtin_is?' do
+    describe 'GTIN-8' do
+      it 'valid' do
+        expect(GtinValidator.what_gtin_is? "95050003").to eq("GTIN-8")
+      end
+      it 'invalid' do
+        expect(GtinValidator.what_gtin_is? "01234567").to be_falsey
+      end
+    end
+    describe 'GTIN-12' do
+      it 'valid' do
+        expect(GtinValidator.what_gtin_is? "012345678905").to eq("GTIN-12")
+      end
+      it 'invalid' do
+        expect(GtinValidator.what_gtin_is? "012345678901").to be_falsey
+      end
+    end
+    describe 'GTIN-13' do
+      it 'valid' do
+        expect(GtinValidator.what_gtin_is? "0123456789012").to eq("GTIN-13")
+      end
+      it 'invalid' do
+        expect(GtinValidator.what_gtin_is? "0123456789000").to be_falsey
+      end
+    end
+    describe 'GTIN-14' do
+      it 'valid' do
+        expect(GtinValidator.what_gtin_is? "00012345600012").to eq("GTIN-14")
+      end
+      it 'invalid' do
+        expect(GtinValidator.what_gtin_is? "00012345678901").to be_falsey
+      end
+    end
+    describe 'without GTIN' do
+      it 'should see false' do
+        expect(GtinValidator.what_gtin_is? "01234").to be_falsey
+      end
+    end
+  end
+
+  describe '#gtin_8?' do
+    it 'corect GTIN should be true' do
+      expect(GtinValidator.gtin_8? "95050003").to be_truthy
+    end
+
+    it 'incorect GTIN should be false' do
+      expect(GtinValidator.gtin_8? "01234567").to be_falsey
+      expect(GtinValidator.gtin_8? "00012345600012").to be_falsey
+    end
+
+    it 'alias' do
+      expect(GtinValidator.ean_8? "95050003").to be_truthy
+      expect(GtinValidator.is_gtin_8? "95050003").to be_truthy
+    end
+  end
+
+  describe '#gtin_12?' do
+    it 'corect GTIN should be true' do
+      expect(GtinValidator.gtin_12? "012345678905").to be_truthy
+    end
+
+    it 'incorect GTIN should be false' do
+      expect(GtinValidator.gtin_12? "012345678900").to be_falsey
+      expect(GtinValidator.gtin_12? "95050003").to be_falsey
+    end
+
+    it 'alias' do
+      expect(GtinValidator.upc? "012345678905").to be_truthy
+      expect(GtinValidator.is_upc? "012345678905").to be_truthy
+      expect(GtinValidator.upc_12? "012345678905").to be_truthy
+      expect(GtinValidator.is_gtin_12? "012345678905").to be_truthy
+    end
+  end
+
+  describe '#gtin_13?' do
+    it 'corect GTIN should be true' do
+      expect(GtinValidator.gtin_13? "0123456789012").to be_truthy
+    end
+
+    it 'incorect GTIN should be false' do
+      expect(GtinValidator.gtin_13? "0123456789001").to be_falsey
+      expect(GtinValidator.gtin_13? "012345678905").to be_falsey
+    end
+
+    it 'alias' do
+      expect(GtinValidator.ean? "0123456789012").to be_truthy
+      expect(GtinValidator.is_ean? "0123456789012").to be_truthy
+      expect(GtinValidator.ean_13? "0123456789012").to be_truthy
+      expect(GtinValidator.is_gtin_13? "0123456789012").to be_truthy
+    end
+  end
+
+  describe '#gtin_14?' do
+    it 'corect GTIN should be true' do
+      expect(GtinValidator.gtin_14? "00012345600012").to be_truthy
+    end
+
+    it 'incorect GTIN should be false' do
+      expect(GtinValidator.gtin_14? "00012345600001").to be_falsey
+      expect(GtinValidator.gtin_14? "0123456789012").to be_falsey
+    end
+
+    it 'alias' do
+      expect(GtinValidator.is_gtin_14? "00012345600012").to be_truthy
+    end
   end
 end
